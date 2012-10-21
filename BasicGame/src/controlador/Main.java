068a297590456908527c7f175b3f7d8fe5bde24e
Sergi <Sergi@Sergi-PC>
2012-10-21 18:55:56 +0200

    Sergi - Classe VehicleProtagonista creada.

--- BasicGame/src/controlador/Main.java
+++ BasicGame/src/controlador/Main.java
@@ -60,8 +60,10 @@ import com.jme3.scene.CameraNode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.control.CameraControl;
 import com.jme3.scene.control.CameraControl.ControlDirection;
 import com.jme3.scene.shape.Cylinder;
+import model.VehicleProtagonista;
 
 
 public class Main extends SimpleApplication implements ActionListener {
@@ -69,11 +71,11 @@ public class Main extends SimpleApplication implements ActionListener {
     private BulletAppState bulletAppState;
     private float steeringValue = 0;
     private float accelerationValue = 0;
-    private VehicleControl vehicle;
+    private VehicleProtagonista car;
     private Spatial sceneModel;
     private final float accelerationForce = 1000.0f;
     private final float brakeForce = 100.0f;
-    private Vector3f jumpForce = new Vector3f(0, 3000, 0);
+    private CameraNode camNode;
 
 
     public static void main(String[] args) {
@@ -82,7 +84,6 @@ public class Main extends SimpleApplication implements ActionListener {
     }
     
     private RigidBodyControl landscape;
-    private CameraNode camNode;
 
     private void setupKeys() {
         inputManager.addMapping("Lefts", new KeyTrigger(KeyInput.KEY_LEFT));
@@ -126,10 +127,29 @@ public class Main extends SimpleApplication implements ActionListener {
 
         setupKeys();
         setUpLight();
-        buildPlayer();
+        
+        car = new VehicleProtagonista(getAssetManager(), getPhysicsSpace(), cam);
+        
+        car.buildCar();
+        
         
         //Añadimos la scena
         rootNode.attachChild(sceneModel);
+        
+        //Añadimos el coche protagonista
+        rootNode.attachChild(car.getSpatial());
+        
+        //Settejem la camera
+        
+        camNode = new CameraNode("CamNode", cam);
+        camNode.setControlDir(CameraControl.ControlDirection.SpatialToCamera);
+        camNode.setLocalTranslation(new Vector3f(0, 4, -15));
+        //camNode.setLocalTranslation(new Vector3f(-15, 15, -15));
+        //camNode.setLocalTranslation(new Vector3f(-15, 15, -15));
+        camNode.lookAt(car.getSpatial().getLocalTranslation(), Vector3f.UNIT_Y);
+        
+        rootNode.attachChild(camNode);
+        
         
         //Añadimos el mundo en la colisiones
         bulletAppState.getPhysicsSpace().add(landscape);
@@ -151,132 +171,6 @@ public class Main extends SimpleApplication implements ActionListener {
         return bulletAppState.getPhysicsSpace();
     }
 
-    private Geometry findGeom(Spatial spatial, String name) {
-        if (spatial instanceof Node) {
-            Node node = (Node) spatial;
-            for (int i = 0; i < node.getQuantity(); i++) {
-                Spatial child = node.getChild(i);
-                Geometry result = findGeom(child, name);
-                if (result != null) {
-                    return result;
-                }
-            }
-        } else if (spatial instanceof Geometry) {
-            if (spatial.getName().startsWith(name)) {
-                return (Geometry) spatial;
-            }
-        }
-        return null;
-    }
-
-    private void buildPlayer() {
-        final float mass = 900;
-        Material mat = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.getAdditionalRenderState().setWireframe(true);
-        mat.setColor("Color", ColorRGBA.Red);
-
-
-        //create a compound shape and attach the BoxCollisionShape for the car body at 0,1,0
-        //this shifts the effective center of mass of the BoxCollisionShape to 0,-1,0
-        CompoundCollisionShape compoundShape = new CompoundCollisionShape();
-        BoxCollisionShape box = new BoxCollisionShape(new Vector3f(1.2f, 0.5f, 2.4f));
-        compoundShape.addChildShape(box, new Vector3f(0, 1, 0));
-
-        //create vehicle node
-        Node vehicleNode = new Node("vehicleNode");
-        vehicle = new VehicleControl(compoundShape, mass);
-        vehicleNode.addControl(vehicle);
-        
-        Node meshNode = (Node) assetManager.loadModel("Models/tempCar/Car.scene");
-        
-        Geometry chasis1 = findGeom(meshNode, "Car");
-        chasis1.rotate(0, 3.135f, 0);
-        //Geometry glass = findGeom(meshNode, "Cube2");
-        
-        //Spatial chasis = (Spatial)assetManager.loadModel("Models/Cube.mesh.xml");
-        vehicleNode.attachChild(chasis1);
-        //vehicleNode.attachChild(glass);
-        
-        //chasis1.scale(1, 1, 1.8f);
-        //glass.scale(1, 1, 1.8f);
-        //chasis.setMaterial(chasisMat);
-        //chasis.setLocalTranslation(0, 0.5f, 0);
-        
-        //setting suspension values for wheels, this can be a bit tricky
-        //see also https://docs.google.com/Doc?docid=0AXVUZ5xw6XpKZGNuZG56a3FfMzU0Z2NyZnF4Zmo&hl=en
-        float stiffness = 60.0f;//200=f1 car
-        float compValue = .3f; //(should be lower than damp)
-        float dampValue = .4f;
-        vehicle.setSuspensionCompression(compValue * 10.0f * FastMath.sqrt(stiffness));
-        vehicle.setSuspensionDamping(dampValue * 2.0f * FastMath.sqrt(stiffness));
-        vehicle.setSuspensionStiffness(stiffness);
-        vehicle.setMaxSuspensionForce(10000.0f);
-
-        //Create four wheels and add them at their locations
-        Vector3f wheelDirection = new Vector3f(0, -1, 0); // was 0, -1, 0
-        Vector3f wheelAxle = new Vector3f(-1, 0, 0); // was -1, 0, 0
-        float radius = 0.5f;
-        float restLength = 0.3f;
-        float yOff = 0.5f;
-        float xOff = 1f;
-        float zOff = 2f;
-
-
-        Node node1 = new Node("wheel 1 node");
-        Geometry wheels1 = findGeom(meshNode, "WheelFrontRight");
-        node1.attachChild(wheels1);
-        wheels1.center();
-        vehicle.addWheel(node1, new Vector3f(-xOff, yOff, zOff),
-                wheelDirection, wheelAxle, restLength, radius, true);
-
-        Node node2 = new Node("wheel 2 node");
-        Geometry wheels2 = findGeom(meshNode, "WheelFrontLeft");
-        node2.attachChild(wheels2);
-        wheels2.center();
-        vehicle.addWheel(node2, new Vector3f(xOff, yOff, zOff),
-                wheelDirection, wheelAxle, restLength, radius, true);
-
-        Node node3 = new Node("wheel 3 node");
-        Geometry wheels3 = findGeom(meshNode, "WheelBackRight");
-        node3.attachChild(wheels3);
-        wheels3.center();
-        vehicle.addWheel(node3, new Vector3f(-xOff, yOff, -zOff),
-                wheelDirection, wheelAxle, restLength, radius, false);
-
-        Node node4 = new Node("wheel 4 node");
-        Geometry wheels4 = findGeom(meshNode, "WheelBackLeft");
-        node4.attachChild(wheels4);
-        wheels4.center();
-        vehicle.addWheel(node4, new Vector3f(xOff, yOff, -zOff),
-                wheelDirection, wheelAxle, restLength, radius, false);
-
-        vehicleNode.attachChild(node1);
-        vehicleNode.attachChild(node2);
-        vehicleNode.attachChild(node3);
-        vehicleNode.attachChild(node4);
-        
-        
-        rootNode.attachChild(vehicleNode);
-        
-        vehicle.getWheel(2).setFrictionSlip(10);
-        vehicle.getWheel(3).setFrictionSlip(10);
-        
-        getPhysicsSpace().add(vehicle);
-        
-        //set forward camera node that follows the character
-        camNode = new CameraNode("CamNode", cam);
-        camNode.setControlDir(ControlDirection.SpatialToCamera);
-        camNode.setLocalTranslation(new Vector3f(0, 4, -15));
-        //camNode.setLocalTranslation(new Vector3f(-15, 15, -15));
-        //camNode.setLocalTranslation(new Vector3f(-15, 15, -15));
-        camNode.lookAt(vehicleNode.getLocalTranslation(), Vector3f.UNIT_Y);
-        vehicleNode.attachChild(camNode);
-       
-         
-        //disable the default 1st-person flyCam (don't forget this!!)
-        flyCam.setEnabled(false);
-    }
-
     public void onAction(String binding, boolean value, float tpf) {
         if (binding.equals("Lefts")) {
             if (value) {
@@ -284,39 +178,35 @@ public class Main extends SimpleApplication implements ActionListener {
             } else {
                 steeringValue += -.5f;
             }
-            vehicle.steer(steeringValue);
+            car.getVehicle().steer(steeringValue);
         } else if (binding.equals("Rights")) {
             if (value) {
                 steeringValue += -.5f;
             } else {
                 steeringValue += .5f;
             }
-            vehicle.steer(steeringValue);
+            car.getVehicle().steer(steeringValue);
         } else if (binding.equals("Ups")) {
             if (value) {
                 accelerationValue += accelerationForce;
             } else {
                 accelerationValue -= accelerationForce;
             }
-            vehicle.accelerate(accelerationValue);
+            car.getVehicle().accelerate(accelerationValue);
         } else if (binding.equals("Downs")) {
             if (value) {
-                vehicle.brake(brakeForce);
+                car.getVehicle().brake(brakeForce);
             } else {
-                vehicle.brake(0f);
-            }
-        } else if (binding.equals("Space")) {
-            if (value) {
-                vehicle.applyImpulse(jumpForce, Vector3f.ZERO);
+                car.getVehicle().brake(0f);
             }
         } else if (binding.equals("Reset")) {
             if (value) {
                 System.out.println("Reset");
-                vehicle.setPhysicsLocation(Vector3f.ZERO);
-                vehicle.setPhysicsRotation(new Matrix3f());
-                vehicle.setLinearVelocity(Vector3f.ZERO);
-                vehicle.setAngularVelocity(Vector3f.ZERO);
-                vehicle.resetSuspension();
+                car.getVehicle().setPhysicsLocation(Vector3f.ZERO);
+                car.getVehicle().setPhysicsRotation(new Matrix3f());
+                car.getVehicle().setLinearVelocity(Vector3f.ZERO);
+                car.getVehicle().setAngularVelocity(Vector3f.ZERO);
+                car.getVehicle().resetSuspension();
             } else {
             }
         }
@@ -324,6 +214,11 @@ public class Main extends SimpleApplication implements ActionListener {
 
     @Override
     public void simpleUpdate(float tpf) {
-        cam.lookAt(vehicle.getPhysicsLocation(), Vector3f.UNIT_Y);
+        flyCam.setEnabled(false);
+        
+        camNode.lookAt(car.getSpatial().getWorldTranslation(), Vector3f.UNIT_Y);
+        
+        camNode.setLocalTranslation(car.getSpatial().localToWorld( new Vector3f( 0, 4, -15), null));
+        
     }
 }

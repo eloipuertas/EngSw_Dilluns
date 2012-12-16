9320ee560b72c77a2cb8a35609299023195d7cc4
José Antonio Cid Montero <jose_antonio_cid@hotmail.com>
2012-12-16 18:03:53 +0100

    Corregido error de la pared invisible, añadidos obtaculos y conos para evitar rutas incorrectas

--- BasicGame/src/model/WorldCreator.java
+++ BasicGame/src/model/WorldCreator.java
@@ -209,6 +209,10 @@ public class WorldCreator {
         //Obstacle creation
         mostrarCaixes();
         
+        if(menu.getIdCircuit() == 0 || menu.getIdCircuit()==2) {
+            mostarCono();
+        }
+        
         if(menu.getDayStateName() == "Noche"){
             mostrarLlums();
         }
@@ -333,7 +337,7 @@ public class WorldCreator {
         obstacleList.add(obstacleModel);
     }
     
-    private void crearCaixaFracturada(int x, int y, int z) {
+    private void crearCaixaFracturada(float x, float y, float z) {
         Spatial caja_parte1 = assetManager.loadModel("Models/caixa_fracturada/superior.j3o");
         caja_parte1.setLocalTranslation(x,y,z);
         caja_parte1.addControl(new RigidBodyControl(5));
@@ -354,22 +358,27 @@ public class WorldCreator {
         space.getPhysicsSpace().add(caja_parte3);
     }
     
+    private void crearCono(float x, float y, float z) {
+        Spatial cono = assetManager.loadModel("Models/cone_joined/cone_joined.j3o");
+        cono.setLocalTranslation(x,y,z);
+        cono.addControl(new RigidBodyControl(3000));
+        cono.getControl(RigidBodyControl.class).setFriction(2f);
+        mundo.attachChild(cono);
+        space.getPhysicsSpace().add(cono);
+    }
+    
     private void initMapas() {
         ArrayList<Vector3f> luces = new ArrayList<Vector3f>();
         ArrayList<Vector3f> cajas = new ArrayList<Vector3f>();
         ArrayList<Vector3f> muros = new ArrayList<Vector3f>();
         ArrayList<Vector3f> medidas = new ArrayList<Vector3f>();
-        cajas.add(new Vector3f(2,-3,-10));
-        cajas.add(new Vector3f(2,-3,-50));
-        cajas.add(new Vector3f(-25,-3,-50));
-        cajas.add(new Vector3f(-50,-3,-50));
-        cajas.add(new Vector3f(-25,-3,-25));
-        cajas.add(new Vector3f(-50,-3,0));
-        cajas.add(new Vector3f(-50,-3,50));
-        cajas.add(new Vector3f(-50,-3,20));
-        cajas.add(new Vector3f(0,-3,50));
-        muros.add(new Vector3f(-2,-5,10));
-        muros.add(new Vector3f(-55,-5,-15));
+        cajas.add(new Vector3f(-33, (float)-5.5, 69));
+        cajas.add(new Vector3f(-40, (float)-5.5, 61));
+        cajas.add(new Vector3f(-35, (float)-5.5, 49));
+        cajas.add(new Vector3f(-40, (float)-5.5, 38));
+        cajas.add(new Vector3f(-39, (float)-5.5, 31));
+        cajas.add(new Vector3f(-43, (float)-5.5, 23));
+        cajas.add(new Vector3f(-38, (float)-5.5, -28));
         luces.add(new Vector3f((float)11.057709,(float) 3.6817183,(float) 70.557945));
         luces.add(new Vector3f((float)-24.116646, (float)3.365127, (float)70.52438));
         luces.add(new Vector3f((float)-23.936317, (float)3.7200565, (float)49.05581));
@@ -411,7 +420,6 @@ public class WorldCreator {
         cajas.add(new Vector3f(-54, -6, -39));
         cajas.add(new Vector3f(-54, -6, -39));
         muros.add(new Vector3f(86, -6, 135));
-        muros.add(new Vector3f(-5, -5, -145));
         luces.add(new Vector3f((float)20.77127, (float)7.9598684, (float)-5.7752357));
         luces.add(new Vector3f((float)20.672327, (float)8.051395, (float)-37.470642));
         luces.add(new Vector3f((float)21.844162, (float)8.54894, (float)-52.538074));
@@ -439,36 +447,19 @@ public class WorldCreator {
         cajas = new ArrayList<Vector3f>();
         muros = new ArrayList<Vector3f>();
         medidas = new ArrayList<Vector3f>();
-        cajas.add(new Vector3f(2,-3,-10));
-        cajas.add(new Vector3f(2,-3,-50));
-        cajas.add(new Vector3f(-25,-3,-50));
-        cajas.add(new Vector3f(-50,-3,-50));
-        cajas.add(new Vector3f(-25,-3,-25));
-        cajas.add(new Vector3f(-50,-3,0));
-        cajas.add(new Vector3f(-50,-3,50));
-        cajas.add(new Vector3f(-50,-3,20));
-        cajas.add(new Vector3f(0,-3,50));
-        muros.add(new Vector3f(-2,-5,10));
-        muros.add(new Vector3f(-55,-5,-15));
-        luces.add(new Vector3f((float)11.057709,(float) 3.6817183,(float) 70.557945));
-        luces.add(new Vector3f((float)-24.116646, (float)3.365127, (float)70.52438));
-        luces.add(new Vector3f((float)-23.936317, (float)3.7200565, (float)49.05581));
-        luces.add(new Vector3f((float)-25.270641, (float)3.7790525, (float)26.0042));
-        luces.add(new Vector3f((float)-46.638527, (float)3.7798347, (float)14.29488));
-        luces.add(new Vector3f((float)-27.151728, (float)3.6546752, (float)3.2493808));
-        luces.add(new Vector3f((float)-7.360218, (float)3.7280383, (float)2.8712435));
-        luces.add(new Vector3f((float)-7.360218, (float)3.7280383, (float)2.8712435));
-        luces.add(new Vector3f((float)11.829636, (float)3.719032, (float)2.9689732));
-        luces.add(new Vector3f((float)10.426862, (float)3.3665524, (float)25.892347));
-        luces.add(new Vector3f((float)10.071195, (float)3.7138824, (float)47.38325));
-        luces.add(new Vector3f((float)30.038078, (float)3.3638551, (float)-8.739162));
-        luces.add(new Vector3f((float)30.82807, (float)3.7592447, (float)-30.193073));
-        luces.add(new Vector3f((float)30.162527, (float)3.7674172, (float)-53.28356));
-        luces.add(new Vector3f((float)29.575834, (float)3.75111, (float)-76.80468));
-        luces.add(new Vector3f((float)-47.46072, (float)3.7272272, (float)-75.62149));
-        luces.add(new Vector3f((float)-47.46072, (float)3.7272272, (float)-75.62149));
-        luces.add(new Vector3f((float)-47.78743, (float)3.3517253, (float)-53.92456));
-        luces.add(new Vector3f((float)-48.309227,(float) 3.7637587, (float)-32.44741));
+        cajas.add(new Vector3f(-33, (float)-3.5, 69));
+        cajas.add(new Vector3f(-40, (float)-3.5, 61));
+        cajas.add(new Vector3f(-35, (float)-3.5, 49));
+        cajas.add(new Vector3f(-40, (float)-3.5, 38));
+        cajas.add(new Vector3f(-39, (float)-3.5, 31));
+        cajas.add(new Vector3f(-43, (float)-3.5, 23));
+        cajas.add(new Vector3f(-38, (float)-3.5, -28));
+        cajas.add(new Vector3f(147, (float)-3.5, -28));
+        muros.add(new Vector3f(148, (float)-3.5, 3));
+        cajas.add(new Vector3f(135, (float)-3.5, 80));
+        cajas.add(new Vector3f(130, (float)-3.5, 79));
+        luces.add(new Vector3f((float)10.994606, (float)4.4260845, (float)70.593994));
+        luces.add(new Vector3f((float)-24.038723, (float)4.4453, (float)70.53151));
         medidas.add(new Vector3f(36, -5, -116));
         medidas.add(new Vector3f(36, -5, 103));
         medidas.add(new Vector3f(-62, -5, 103));
@@ -494,8 +485,40 @@ public class WorldCreator {
         for(int i = 0; i < cajas.size();i++) {
             Vector3f v = cajas.get(i);
             //crearCaixa((int)v.x,(int)v.y,(int)v.z);
-            System.out.println("POSICIONES "+(int)v.x+ " "+(int)v.y+" " + (int)v.z);
-            crearCaixaFracturada((int)v.x,(int)v.y,(int)v.z);
+            System.out.println("POSICIONES "+(int)v.x+ " "+v.y+" " + (int)v.z);
+            crearCaixaFracturada(v.x,v.y,v.z);
+        }
+    }
+    
+    public void mostarCono() {
+        
+        if(menu.getIdCircuit() == 0) {
+            crearCono((float)-28.723972, (float)-6.0622463, (float)18.410522);
+            crearCono((float)-28.372177, (float)-6.0387764, (float)13.971759);
+            crearCono((float)-28.034878, (float)-6.0236855, (float)11.280115);
+            crearCono((float)-28.43544, (float)-6.006256, (float)7.009533); 
+            crearCono((float)14.614142, (float)-5.7505264, (float)16.295588);
+            crearCono((float)14.610315, (float)-5.738961, (float)13.846985);
+            crearCono((float)14.657426, (float)-5.7277317, (float)11.543973);
+            crearCono((float)14.029064, (float)-5.7092752, (float)6.7220063);
+        }
+        if(menu.getIdCircuit() == 2) {
+            crearCono((float)13.465805, (float)-4.340721, (float)18.19282);
+            crearCono((float)13.465248, (float)-4.3407173, (float)14.394462);
+            crearCono((float)13.460812, (float)-4.3407135, (float)9.763995);
+            crearCono((float)13.713562, (float)-4.3407135, (float)5.9770856);
+            crearCono((float)-27.897057, (float)-4.3407173, (float)5.5633893);
+            crearCono((float)-28.199787, (float)-4.3407173, (float)8.728756);
+            crearCono((float)-28.092594, (float)-4.3407154, (float)13.223505);
+            crearCono((float)-27.631493, (float)-4.340719, (float)17.281307);
+            crearCono((float)28.767302, (float)-4.3407536, (float)71.20535);
+            crearCono((float)24.597708, (float)-4.3407516, (float)71.60649);
+            crearCono((float)21.288177, (float)-4.3407516, (float)71.67496);
+            crearCono((float)16.876785, (float)-4.3407497, (float)72.198204);
+            crearCono((float)29.017439, (float)-4.3406706, (float)-65.76872);
+            crearCono((float)24.469894, (float)-4.3406715, (float)-65.52343);
+            crearCono((float)20.667267, (float)-4.3406715, (float)-65.538124);
+            crearCono((float)17.250578, (float)-4.3406715, (float)-66.23939);
         }
     }
     
